ce67a81b9c6ff6cf87b02ef6eddab3cba0b19770
livmeierx <liv.meier13@gmail.com>
2022-09-05 08:10:18 -0400

    completed user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,14 +46,42 @@ module.exports = {
     },
 // Delete a user + BONUS: remove a user's associated thoughts when deleted
     deleteUser(req, res) {
-
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) =>
+        !user
+            ? res.status(404).json({ message: "No User found with this id" })
+            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+        )
+        .then(() => res.json({ message: "User and Thought deleted" }))
+        .catch((err) => res.status(500).json(err));
     },
 // Add a friend
     addFriend(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+        !user
+            ? res.status(404).json({ message: "No User found with this Id" })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
     },
 // Delete a friend
     deleteFriend(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $ull: { friends: req.params.friendId } },
+            { new: true }
+        )
+        .then(
+            (user) =>
+            !user
+                ? res.status(404).json({ message: "No User found with this Id" })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
     },        
 };
